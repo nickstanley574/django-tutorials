2897bb7e76fafa4641b3af816d7a7d10ff21a3d1
nicholasstanley574 <40447503+nicholasstanley574@users.noreply.github.com>
2018-07-02 18:22:50 -0500

    Django tutorial part 7,updated admin to be able to add choice to question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# This tells Django: “Choice objects are edited on the 
+# Question admin page. By default, provide enough fields 
+# for 3 choices.”
+#  For that reason, Django offers a tabular way of displaying inline related objects;
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date','question_text']
+    fieldsets = [
+            (None,               {'fields':['question_text']}),
+            ('Date information', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
